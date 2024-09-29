61b1535a475b28bd952bfcb26a7f529728ff9e6f
Paul OHL <paul.lv.ohl@gmail.com>
2024-09-29 21:15:50 +0200

    More custom gnome settings!

--- kanata/.config/kanata/gnome-shortcuts.sh
+++ kanata/.config/kanata/gnome-shortcuts.sh
@@ -25,9 +25,21 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Con
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Shift><Super>q']"
 gsettings set org.gnome.desktop.wm.keybindings cycle-windows "['<Super>Space']"
+gsettings set org.gnome.desktop.wm.keybindings cycle-windows-backward "['<Shift><Super>Space']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "[]"
 
 # Set settings
+gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface enable-hot-corners "false"
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface clock-format "24h"
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
+gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state true
+gsettings set org.gnome.shell favorite-apps "['kitty.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop', 'com.spotify.Client.desktop']"
+gsettings set org.gnome.shell.weather locations "[<(uint32 2, <('Paris', 'LFPB', true, [(0.85462956287765413, 0.042760566673861078)], [(0.8528842336256599, 0.040724343395436846)])>)>]"
+gsettings set org.gnome.shell.world-clocks locations "[<(uint32 2, <('Montreal', 'CYUL', true, [(0.79354303905785273, -1.2871803233458181)], [(0.79354303905785273, -1.2871803233458181)])>)>, <(uint32 2, <('London', 'EGWU', true, [(0.89971722940307675, -0.007272211034407213)], [(0.89884456477707964, -0.0020362232784242244)])>)>]"
