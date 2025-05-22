7722aa9eb8386c58f2868145ceedc7bd54e0f22b
Paul OHL <paul.lv.ohl@gmail.com>
2025-05-22 18:39:51 +0200

    changed back to kitty and added a telescope binding

--- gnome/gnome-shortcuts.sh
+++ gnome/gnome-shortcuts.sh
@@ -33,7 +33,7 @@ gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
 gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
 gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state true
-gsettings set org.gnome.shell favorite-apps ['Alacritty.desktop', 'app.zen_browser.zen.desktop', 'com.discordapp.Discord.desktop', 'md.obsidian.Obsidian.desktop', 'com.spotify.Client.desktop', 'org.gnome.Nautilus.desktop']
+gsettings set org.gnome.shell favorite-apps ['kitty.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop', 'com.spotify.Client.desktop']
 gsettings set org.gnome.shell.weather locations "[<(uint32 2, <('Paris', 'LFPB', true, [(0.85462956287765413, 0.042760566673861078)], [(0.8528842336256599, 0.040724343395436846)])>)>]"
 gsettings set org.gnome.shell.world-clocks locations "[<(uint32 2, <('Montreal', 'CYUL', true, [(0.79354303905785273, -1.2871803233458181)], [(0.79354303905785273, -1.2871803233458181)])>)>, <(uint32 2, <('London', 'EGWU', true, [(0.89971722940307675, -0.007272211034407213)], [(0.89884456477707964, -0.0020362232784242244)])>)>]"
 gsettings set org.gnome.desktop.interface accent-color 'green'
--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -47,6 +47,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[R]esume last search' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = '[S]earch [B]uffers' })
+      vim.keymap.set('n', 'l', builtin.buffers, { desc = '[S]earch [B]uffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
