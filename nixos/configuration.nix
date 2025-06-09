b1dc63834ff702520dd9b26c6002cf49e9dacf24
Paul OHL <paul.lv.ohl@gmail.com>
2025-06-09 12:19:39 +0200

    13 current  2025-06-09 12:19:37  25.05.803579.70c74b02eac4  6.12.32                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -180,26 +180,32 @@
           # uBlock Origin:
           "uBlock0@raymondhill.net" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "normal_installed";
           };
           # Bitwarden:
           "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "normal_installed";
           };
           # Vimium:
           "vimium-c@gdh1995.cn" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-c/latest.xpi";
+            installation_mode = "normal_installed";
           };
           # I still don't care about cookies:
           "idcac-pub@guus.ninja" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/latest.xpi";
+            installation_mode = "normal_installed";
           };
           # Dark reader
           "addon@darkreader.org" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+            installation_mode = "normal_installed";
           };
           # Sidebery
           "{3c078156-979c-498b-8990-85f7987dd929}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
+            installation_mode = "normal_installed";
           };
         };
 
