6ac4b086379891b203d495b9b7d04610e750703f
Paul OHL <paul.lv.ohl@gmail.com>
2025-06-09 12:00:48 +0200

    11 current  2025-06-09 12:00:46  25.05.803579.70c74b02eac4  6.12.32                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -174,7 +174,7 @@
         # Valid strings for installation_mode are "allowed", "blocked",
         # "force_installed" and "normal_installed".
         ExtensionSettings = {
-          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+          "*".installation_mode = "normal_installed"; # blocks all addons except the ones specified below
           # uBlock Origin:
           "uBlock0@raymondhill.net" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
