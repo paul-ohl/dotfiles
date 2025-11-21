4cd458e8205456a4e6c25268a1f5a391a3c99ab3
Paul OHL <paul.lv.ohl@gmail.com>
2025-11-21 16:51:33 +0100

    feat: added ssh-daemon module for cornifer

---
+++ nixos/modules/ssh-daemon.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, inputs, username, ... }:
+
+{
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      # AllowUsers = [ username ];
+      # UseDns = true;
+      # X11Forwarding = false;
+      PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+}
