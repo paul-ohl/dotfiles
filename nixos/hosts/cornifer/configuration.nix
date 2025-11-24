da5fd0a87ab721627f9d4bc542c26887679e4354
Paul OHL <paul.lv.ohl@gmail.com>
2025-11-24 13:19:29 +0100

    fix: Specific cornifer updates

--- nixos/hosts/cornifer/configuration.nix
+++ nixos/hosts/cornifer/configuration.nix
@@ -9,6 +9,18 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  networking.firewall = {
+    allowedTCPPorts = [ 80 443 8096 4533 4534 ];
+  };
+
+  # Disable system suspend
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    hybrid-sleep.enable = false;
+  };
+
   # Host-specific packages
   environment.systemPackages = with pkgs; [
     # Add packages specific to this machine
