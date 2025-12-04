e0fb105455d76f49865b26278a71332f12585aad
Paul OHL <paul.lv.ohl@gmail.com>
2025-12-04 11:14:17 +0100

    fix: Updates to syncthing

--- nixos/modules/syncthing.nix
+++ nixos/modules/syncthing.nix
@@ -5,10 +5,20 @@
     enable = true;
     extraFlags = [ "--no-default-folder" ];
     openDefaultPorts = true;
+    guiAddress = "0.0.0.0:8384";
+    configDir = "/home/astro/.config/syncthing";
+    user="astro";
+    group="users";
+
     settings = {
+      gui = {
+        tls = false;
+      };
+
       devices = {
         "hornet".id = "CEGTXKA-GE57XYN-6MRJ3OK-6QGTQ27-DO54MEN-KLQL4RV-MPKYB2G-NP44ZQ6";
       };
+
       folders = {
         "org" = {
           path = "/home/astro/Documents/org/";
@@ -21,4 +31,7 @@
       };
     };
   };
+
+  # Open the Syncthing GUI port
+  networking.firewall.allowedTCPPorts = [ 8384 ];
 }
