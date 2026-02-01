564f18b8ed110d0b93dee2dfaa2433754c6aa147
Paul OHL <paul.lv.ohl@gmail.com>
2026-02-01 22:26:18 +0100

    fix: Better org config

--- neovim/.config/nvim/lua/plugins/markdown.lua
+++ neovim/.config/nvim/lua/plugins/markdown.lua
@@ -6,6 +6,33 @@ return {
   opts = {},
   config = function()
     vim.keymap.set('n', '<leader>um', '<Cmd>RenderMarkdown toggle<CR>', { desc = 'Toggle Markdown rendering' })
+
+    -- Toggle todo item with Ctrl+Space
+    vim.keymap.set('n', '<C-Space>', function()
+      local line = vim.api.nvim_get_current_line()
+      local new_line
+
+      if line:match '%- %[ %]' then
+        -- Unchecked todo -> checked todo
+        new_line = line:gsub('%- %[ %]', '- [x]', 1)
+      elseif line:match '%- %[[xX]%]' then
+        -- Checked todo (x or X) -> unchecked todo
+        new_line = line:gsub('%- %[[xX]%]', '- [ ]', 1)
+      elseif line:match '^(%s*)[%-%*]%s' then
+        -- List item (- or *) -> unchecked todo
+        new_line = line:gsub('^(%s*)[%-%*]%s', '%1- [ ] ', 1)
+      elseif line:match '%S' then
+        -- Non-empty line with no match -> prepend unchecked todo
+        local indent = line:match '^(%s*)'
+        local content = line:gsub('^%s*', '')
+        new_line = indent .. '- [ ] ' .. content
+      else
+        return
+      end
+
+      vim.api.nvim_set_current_line(new_line)
+    end, { desc = 'Toggle todo item' })
+
     require('render-markdown').setup {
       patterns = {
         markdown = {
@@ -17,11 +44,22 @@ return {
         },
       },
       anti_conceal = {
-        enabled = false,
+        enabled = true,
         ignore = {
           code_background = true,
           code_border = false,
           sign = true,
+          bullet = true,
+          checkbox = true,
+          dash = true,
+          quote = true,
+          table_border = true,
+          callout = true,
+          -- Allow anti-conceal for links and headings
+          link = false,
+          head_icon = false,
+          head_background = false,
+          head_border = false,
         },
         above = 0,
         below = 0,
@@ -38,6 +76,11 @@ return {
         enabled = true,
         -- Distinct list icons using arrows/chevrons
         icons = { '➤', '➜', '▹' },
+      },
+      checkbox = {
+        enabled = true,
+        unchecked = { icon = ' -  ' },
+        checked = { icon = ' -  ' },
       },
       link = {
         enabled = true,
